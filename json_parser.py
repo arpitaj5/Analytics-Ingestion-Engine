13e3f8870b15e77b5fdf93f42570c037b6f2486f
avikas <avikas@avikas-03479.evbqa.com>
2018-01-31 17:30:25 -0800

    error handling

--- json_parser.py
+++ json_parser.py
@@ -1,14 +1,22 @@
 import os
 import json
-prefix = 'lala'
+prefix = sys.argv[1]
+
 prefixed = [filename for filename in os.listdir('srv/runme') if filename.startswith(prefix)]
 
+# if there are no files with prefix name. Exit code
+if not prefixed:
+	print "No file starts with this name"
+	exit()
 
 name = list()
 age = list()
 for filename in prefixed:
 	with open('srv/runme/'+filename, 'r') as f:
-	    datastore = json.load(f)
+		try:
+	    	datastore = json.load(f)
+	    except:
+	    	pass
 	for i in range(len(datastore)):
 		name.append(datastore[i]['name'])
 		age.append(datastore[i]['prop']['age'])
