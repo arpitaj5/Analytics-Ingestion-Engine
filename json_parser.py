99b72df5c066583f988e1824a4c0c532f88c876a
Asmita Vikas <vikasmita20@gmail.com>
2018-02-01 12:11:29 -0800

    bugfix and permission issues

--- json_parser.py
+++ json_parser.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 prefix = sys.argv[1]
 
 print "checking files with prefix"
@@ -7,22 +8,27 @@ prefixed = [filename for filename in os.listdir('/srv/runme') if filename.starts
 
 # if there are no files with prefix name. Exit code
 if not prefixed:
-	print "No file starts with this name"
-	exit()
+    print "No file starts with this name"
+    exit()
 
 name = list()
 age = list()
 for filename in prefixed:
-	with open('/srv/runme/'+filename, 'r') as f:
-		try:
-	    	datastore = json.load(f)
-	    except:
-	    	pass
-	for i in range(len(datastore)):
-		name.append(datastore[i]['name'])
-		age.append(datastore[i]['prop']['age'])
+    with open('/srv/runme/'+filename, 'r') as f:
+        try:
+            datastore = json.load(f)
+        except:
+            pass
+    print len(datastore)
+    for i in range(len(datastore)):
+        name.append(datastore[i]['name'])
+        age.append(datastore[i]['prop']['age'])
 
-fh = open("/srv/runme/"+prefix+".txt", "w")
+fh = open(prefix+".txt", "w")
 for i in range(len(name)):
-	fh.write(name[i] + "\t" + str(age[i])+"\n")
+    fh.write(name[i] + "\t" + str(age[i])+"\n")
 fh.close()
+
+
+
+os.system('sudo mv ' + prefix + '.txt /srv/runme/')
