bf274353dcf410031ff6280a1e1f1e8e4d646f05
nis89mad <nishan.madawanarachchi@gmail.com>
2018-02-11 16:04:37 -0800

    minor

--- deploy_script.py
+++ deploy_script.py
@@ -12,13 +12,15 @@ def deploy(path, server, prefix):
   print "Connecting to box"
   ssh = paramiko.SSHClient()
   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  print server
+  print path
   ssh.connect(server, username = 'ec2-user', key_filename = path)
   print "Connected to server"
 
   # clone git
   print "cloning git"
   ssh.exec_command("rm -rf Analytics-Ingestion-Engine; git clone https://github.com/asmitav/Analytics-Ingestion-Engine.git")
-  ssh.exec_command("cd Analytics-Ingestion-Engine; git checkout json_parser_nishan2")
+  ssh.exec_command("cd Analytics-Ingestion-Engine; git checkout origin/json_parser_nishan2")
   print "cloned git"
 
   print "crontab remove"
