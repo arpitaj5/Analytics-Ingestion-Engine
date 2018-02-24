3687d93a20dde75156619c2466e58e0f127c083d
arpitaj5 <ajena@dons.usfca.edu>
2018-02-23 22:07:15 -0800

    json updated

--- json_parser.py
+++ json_parser.py
@@ -3,30 +3,12 @@ import json
 import sys
 import glob
 import logging
-import time 
+import time
 from logging.handlers import TimedRotatingFileHandler
 
-def create_timed_rotating_log(path, name_age):
-    """
-    PATH: global variable - 
-    
-    
-    logger = logging.getLogger("Rotating Log")
-    logger.setLevel(logging.INFO)
-    handler = TimedRotatingFileHandler(path,
-                                       when="m",
-                                       interval=2)
-    logger.addHandler(handler)
-    logger.info(name_age)
-    """
-
-#def main():
-try:
-    prefix = sys.argv[1]
-except:
-    print "Error: No argument passed!!"
-
-PATH = "/srv/runme/" + prefix + "/proc.txt"
+prefix = sys.argv[1]
+dir = "/srv/runme/" + prefix
+PATH = dir + "/proc.txt"
 
 if glob.glob(PATH+"*") != []:
     for f in glob.glob(PATH+"*"):
@@ -37,30 +19,29 @@ logging_level = logging.DEBUG
 formatter = logging.Formatter()
 handler = logging.handlers.TimedRotatingFileHandler(PATH, when="M", interval=2, backupCount=10)
 handler.setFormatter(formatter)
-logger = logging.getLogger("Rotating Log") # or pass string to give it a name
+logger = logging.getLogger("Rotating Log")
 logger.addHandler(handler)
 logger.setLevel(logging_level)
 
-for filename in glob.glob("/srv/runme/" + prefix + "/*"):
-    if 'Raw.txt' in filename:
-        with open(filename, 'r') as f:
-            try:
-                file = f.readlines()
-            except:
-                pass
-        print len(file)
-        for line in file:
-            try:
-                j_line = json.loads(line)
-                if ('name' in j_line) and ('prop' in j_line) and ('age' in j_line['prop']) and (j_line['prop']['age'] > 0):
-                    
-                    name_age = j_line['name'] + "\t" + str(j_line['prop']['age'])
-                    with open(PATH, 'a'):
-                        logger.debug(name_age)                   
-            except:
-                pass
 
-#if (__name__ ==' __main__'):
-#    main()
+files = [filename for filename in glob.glob(dir + "/*") if 'Raw.txt' in filename]
+for filename in files:
+    with open(filename, 'r') as f:
+        try:
+            file = f.readlines()
+        except:
+            print('Bad input!!')
 
-
+    name_age = []
+    for line in file:
+        try:
+            j_line = json.loads(line)
+            name = j_line['name']
+            age = j_line['prop']['age']
+            if (age > 0):
+                name_age.append(name + "\t" + str(age))
+        except:
+            pass
+    with open(PATH, 'a'):
+        for name_age_str in name_age:
+            logger.debug(name_age_str)
