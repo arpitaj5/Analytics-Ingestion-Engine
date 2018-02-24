835cbc7d886b773e448d7bed35b2d71ee2beb332
arpitaj5 <ajena@dons.usfca.edu>
2018-02-23 20:35:42 -0800

    updating json

--- json_parser.py
+++ json_parser.py
@@ -3,75 +3,82 @@ import json
 import sys
 import glob
 import logging
-import time
- 
+import time 
 from logging.handlers import TimedRotatingFileHandler
 
-def create_timed_rotating_log(name_age):
+#def create_timed_rotating_log(path, name_age):
     """
     PATH: global variable - 
     
-    """
-    logger.info(name_age)
-
-
-try:
-    prefix = sys.argv[1]
-except:
-    print "Error: No argument passed!!"
-
-
-#print "checking files with prefix"
-#prefixed = [filename for filename in os.listdir('/srv/runme') if filename.startswith(prefix)]
-
-# if there are no files with prefix name. Exit code
-#if not prefixed:
- #   print "No file starts with this name"
-  #  exit()
-
-PATH = "/srv/runme/" + prefix + "/proc.txt"
-
-print "/srv/runme/" + prefix
-#print glob.glob("/srv/runme/" + prefix)
-
-#if(os.path.exists(PATH+"*")):
-
-if glob.glob(PATH+"*") != []:
-    for f in glob.glob(PATH+"*"):
-        os.remove(f)
-
-
-logger = logging.getLogger("Rotating Log")
-logger.setLevel(logging.INFO)
-handler = TimedRotatingFileHandler(PATH,
+    
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler(path,
                                        when="m",
-                                       interval=2
-                                       )
-logger.addHandler(handler)
+                                       interval=2)
+    logger.addHandler(handler)
+    logger.info(name_age)
+    """
 
-for filename in glob.glob("/srv/runme/" + prefix + "/*"):
-    print filename
-    if 'Raw.txt' in filename:
-        print "Found file" + filename
-        with open(filename, 'r') as f:
-            try:
-                file = f.readlines()
-            except:
-                pass
-        print len(file)
-        for line in file:
-            try:
-                j_line = json.loads(line)
-                print j_line
-                if ('name' in j_line) and ('prop' in j_line) and ('age' in j_line['prop']) and (j_line['prop']['age'] > 0):
-                    
-                    name_age = j_line['name'] + "\t" + str(j_line['prop']['age'])+"\n"
-                    print name_age
-                    create_timed_rotating_log(name_age)
-                    
-            except:
-                pass
+def main():
+    try:
+        prefix = sys.argv[1]
+    except:
+        print "Error: No argument passed!!"
+
+    PATH = "/srv/runme/" + prefix + "/proc.txt"
+
+    if glob.glob(PATH+"*") != []:
+        for f in glob.glob(PATH+"*"):
+            os.remove(f)
 
 
-#os.system('mv /home/testtest/Analytics-Ingestion-Engine/' + prefix + '.txt /srv/runme/')
+    logging_level = logging.DEBUG
+
+    formatter = logging.Formatter()
+
+    handler = logging.handlers.TimedRotatingFileHandler(PATH, when="M", interval=2, backupCount=10)
+
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("Rotating Log") # or pass string to give it a name
+
+    logger.addHandler(handler)
+
+    logger.setLevel(logging_level)
+
+    '''
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler(PATH,
+                                        when="m",
+                                        interval=2)
+    logger.addHandler(handler)
+    '''
+    for filename in glob.glob("/srv/runme/" + prefix + "/*"):
+        print filename
+        if 'Raw.txt' in filename:
+            print "Found file" + filename
+            with open(filename, 'r') as f:
+                try:
+                    file = f.readlines()
+                except:
+                    pass
+            print len(file)
+            for line in file:
+                try:
+                    j_line = json.loads(line)
+                    print j_line
+                    if ('name' in j_line) and ('prop' in j_line) and ('age' in j_line['prop']) and (j_line['prop']['age'] > 0):
+                        
+                        name_age = j_line['name'] + "\t" + str(j_line['prop']['age'])
+                        with open(PATH, 'a'):
+                            print name_age
+                            logger.debug(name_age)                   
+                except:
+                    pass
+
+if (__name__ ==' __main__'):
+    main()
+
 
